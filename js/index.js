8ffc57adab5ff2529278b5a74806ed23f2026d4f
dragance <dragoslavbabic1971@gmail.com>
2020-06-30 15:30:13 +0200

    dalje

--- js/index.js
+++ js/index.js
@@ -1,110 +1,138 @@
-class Kartica{
-    constructor(opis,min,max){
-        this.opis = opis;
-        this.min = min;
-        this.max = max;
+class Kartica {
+  constructor (opis, min, max) {
+    this.opis = opis;
+    this.min = min;
+    this.max = max;
+  }
+
+  generisiKarticu () {
+    const [opis, operacijax] = this.opisOperacija ();
+    let operacija =
+      operacijax +
+      '<div class="form-group sab">' +
+      '<input type="text" class="form-control ">' +
+      '<label for="usr">Upisi rezultat!</label>' +
+      '<div class="dugme">' +
+      '<button onclick="getVal()" class="btn btn-primary">Potvrdi odgovor!</button>' +
+      '</div>' +
+      '<div class="odgovor">' +
+      '<p >Tvoj odgovor je: ' +
+      '<span id="potvrdi_odgovor">' +
+      '</span>' +
+      '</p>' +
+      '</div>' +
+      '</div>';
+
+    let el =
+      '<div class="card col" style="width: 18rem;">' +
+      '<div class="card-body rand">' +
+      '<h5 class="card-title">ZADATAK:</h5>' +
+      '<p class="card-text">' +
+      opis +
+      '</p>' +
+      operacija;
+    '</div>' + '</div>';
+    return el;
+  }
+
+  opisOperacija () {
+    let opis = this.opis;
+    return opis == 'sabiranje'
+      ? ['Saberi sledeće brojeve:', this.sabiranje ()]
+      : opis == 'oduzimanje'
+          ? ['Oduzmi sledeće brojeve:', this.oduzimanje ()]
+          : opis == 'mnozenje'
+              ? 'Pomnoži sledeće brojeve:'
+              : opis == 'deljenje'
+                  ? 'Podeli sledeće brojeve:'
+                  : ' Izračunaj obim i površinu pravoguanika ako su date stranice a = 19 cm i stranice b = 19 cm';
+  }
+
+  rndNiz (l, g, h) {
+    let niz = [];
+    for (const key in Array.from ({length: l})) {
+      let r = this.rndBroj (g, h);
+      niz.push (r);
     }
+    niz.sort (function (a, b) {
+      return b - a;
+    });
+    return niz;
+  }
 
-    generisiKarticu(){
-        const [opis, operacijax] = this.opisOperacija();
-        let operacija = 
-        operacijax+
-        '<div class="form-group sab">'+
-        '<input type="text" class="form-control ">'+
-        '<label for="usr">Upisi rezultat!</label>'+
-        '<div class="dugme">'+
-        '<button onclick="getVal()" class="btn btn-primary">Potvrdi odgovor!</button>'+
-        '</div>'+
-        '<div class="odgovor">'+
-        '<p >Tvoj odgovor je: '+
-        '<span id="potvrdi_odgovor">'+
-        '</span>'+
-        '</p>'+
-        '</div>'+
-        '</div>';
-        
-        let el =
-        '<div class="card col" style="width: 18rem;">'+
-          '<div class="card-body rand">'+
-            '<h5 class="card-title">ZADATAK:</h5>'+
-            '<p class="card-text">'+opis+'</p>'+
-            operacija;
-          '</div>'+
-        '</div>';
-        return el;
-        
-    }
+  sabiranje () {
+    let niz = this.rndNiz (3, this.min, this.max);
+    let x =
+      '<p><span> &nbsp;</span>' +
+      '<span id="broj_1">' +
+      niz[0] +
+      '</span><br>' +
+      '+<span id="broj_2">' +
+      niz[1] +
+      '</span><br>' +
+      '+<span id="broj_3">' +
+      niz[2] +
+      '</span></p><hr>';
+    return x;
+  }
 
-    opisOperacija(){
-        let opis = this.opis;
-        return opis == 'sabiranje' ? ['Saberi sledeće brojeve:', this.sabiranje()]
-        : opis == 'oduzimanje' ?['Oduzmi sledeće brojeve:', this.oduzimanje()]
-        : opis == 'mnozenje' ? 'Pomnoži sledeće brojeve:'
-        : opis == 'deljenje' ?'Podeli sledeće brojeve:'
-        :' Izračunaj obim i površinu pravoguanika ako su date stranice a = 19 cm i stranice b = 19 cm';
+  oduzimanje () {
+    let niz = this.rndNiz (2, this.min, this.max);
+    let x =
+      '<p><span>&nbsp</span>' +
+      '<span id="broj_1">' +
+      niz[0] +
+      '</span><br>' +
+      '<span>-</span><span id="broj_2">' +
+      niz[1] +
+      '</span><br>';
+    return x;
+  }
 
-    }
+  rndBroj = (g, h) => Math.floor (Math.random () * (h - g + 1)) + g;
 
-    rndNiz(l,g,h){
-        let niz = [];
-        for(const key in Array.from({length:l})){
-            let r = this.rndBroj(g,h);
-            niz.push(r);
-        }
-        niz.sort(function(a, b){return b-a});
-        return niz;
-    }
-    
-    sabiranje(){
-        let niz = this.rndNiz(3,this.min,this.max);
-        let x = 
-        '<p><span> &nbsp;</span>'+
-        '<span id="broj_1">'+niz[0]+'</span><br>'+
-        '+<span id="broj_2">'+niz[1]+'</span><br>'+
-        '+<span id="broj_3">'+niz[2]+'</span></p><hr>';
-        return x;
-    }
+  proveriRezultat (x, y) {
+    return x == y ? 'Odgovor je tacan!!' : 'Odgovor nije tacan!!!';
+  }
 
-    oduzimanje(){
-        let niz = this.rndNiz(2,this.min,this.max);
-        let x = 
-        '<p><span>&nbsp</span>'+
-        '<span id="broj_1">'+niz[0]+'</span><br>'+
-        '<span>-</span><span id="broj_2">'+niz[1]+'</span><br>'
-        return x;
-    }
+  oceniMe () {
+    let recenica = 'Odgovor je tacan!!';
+    let tacan_odgovor = ocena.reduce ((n, x) => n + (x === recenica), 0);
+    let tekst =
+      '<div class="col-12 text-center oceni_me">' +
+      '<h4>Ocena je ' +
+      this.prosek (tacan_odgovor) +
+      '</h4' +
+      '</div>';
+    return tekst;
+  }
 
-
-    rndBroj=(g,h)=> Math.floor(Math.random()*(h-g+1))+g;
-
-    proveriRezultat(x,y){
-            return x ==y ? 'Odgovor je tacan!!' :'Odgovor nije tacan!!!';
-    }
-
-    oceniMe(){
-        let recenica = 'Odgovor je tacan!!';
-        let count = ocena.reduce((n, x) => n + (x === recenica), 0);
-        
-        return count;
-    }
-
+  prosek (x) {
+    let prosek = x / 5;
+    return prosek < 0.4
+      ? 1
+      : prosek > 0.4 && prosek < 0.54
+          ? 2
+          : prosek > 0.55 && prosek < 0.69
+              ? 3
+              : prosek > 0.70 && prosek < 0.84 ? 4 : 5 + '! BRAVO!!!!';
+  }
 }
 
 class Proveri {
-    constructor(zadatak,brojevi,rez,tacan_rez){
-        this._zadatak = zadatak;
-        this._brojevi = brojevi;
-        this._rez = rez;
-        this._tacan_rez = tacan_rez;
-    }
+  constructor (zadatak, brojevi, rez, tacan_rez) {
+    this._zadatak = zadatak;
+    this._brojevi = brojevi;
+    this._rez = rez;
+    this._tacan_rez = tacan_rez;
+  }
 }
 
 class KarticaPodaci extends Array {
-    constructor(zadatak,brojevi,rez){
-        super()
-        this._zadatak = zadatak;
-        this._brojevi = brojevi;
-        this._rez = rez;
-
-    }
+  constructor (zadatak, brojevi, rez) {
+    super ();
+    this._zadatak = zadatak;
+    this._brojevi = brojevi;
+    this._rez = rez;
+  }
 }
--- js/sabiranje.js
+++ js/sabiranje.js
@@ -1,71 +1,79 @@
-const arr=[];
+const proveri_div =
+  '<div class="col-12 text-center proveri_rezultate">' +
+  '<button onclick="proveri(arr)" id="proveri_rezultat" class="btn btn-primary">Proveri!</button>' +
+  '</div>';
+const arr = [];
 const ocena = [];
+function GenerisiZadatke () {
+  arr.length = 0;
 
-const proveri_div =    
-'<div class="col-12 text-center proveri_rezultate">'+
-'<button onclick="proveri(arr)" id="proveri_rezultat" class="btn btn-primary">Proveri!</button>'+
-'</div>';
+  ocena.length = 0;
+  $ ('#appendme,.proveri_rezultate,.zadaci_sadrzaj,.rezultati').html ('');
+  let minmax = $ ('input[name="cifre"]:checked').val ();
+  let min = parseInt (minmax.split (',')[0]);
+  let max = parseInt (minmax.split (',')[1]);
+  console.log (min);
+  let y = new Kartica ('sabiranje', min, max);
+  let r;
 
-function GenerisiZadatke(){
-    $('#appendme,.proveri_rezultate,.zadaci_sadrzaj,.rezultati').html('');
-    let minmax = $('input[name="cifre"]:checked').val();
-    let min = parseInt( minmax.split(',')[0]);
-    let max = parseInt(minmax.split(',')[1]);
-    console.log(min)
-    let y = new Kartica("sabiranje",min,max);
-    let r;
-
-for(const key in Array.from({length:5})){
-    r = y.generisiKarticu();
-    $('#appendme').append(r);
-}
-$('.rezultati').append(proveri_div);
-//console.log(r);
-}
-
-
-function getVal(){
-    $('.dugme').unbind().on('click','button',function() {
-        let rez = $(this).closest('div.sab').find('input').val();
-        $(this).closest('div.sab').find('#potvrdi_odgovor').text(rez);
-        let broj1 = Number( $(this).closest('div.rand').find('#broj_1').text());
-        let broj2 = Number( $(this).closest('div.rand').find('#broj_2').text());
-        let broj3 = Number($(this).closest('div.rand').find('#broj_3').text());
-        let objekat = new KarticaPodaci(arr.length+1,[broj1,broj2,broj3],rez);
-        arr.push(objekat);
-    });
-}
-
-function getSum(total, num) {
-    return total + Math.round(num);
+  for (const key in Array.from ({length: 5})) {
+    r = y.generisiKarticu ();
+    $ ('#appendme').append (r);
   }
+  $ ('.rezultati').append (proveri_div);
+  //console.log(r);
+}
 
-function proveri(x) {
-    let k = new Kartica;
-    x.forEach(element => {
-        let x = element._brojevi.reduce(getSum,0);
-        let y = element._rez;
-        //let z = uporedi(x,y);
-        let w = k.proveriRezultat(x,y);
-        let rez_card = 
-        '<div class="zadaci_sadrzaj card col " style="width: 18rem;">'+
-        '<h4>Zadatak broj '+ element._zadatak+'</h4>'+
-        '<p>Brojevi za sabiranje su: ('+ element._brojevi+')</p>'+
-        '<p>Tacan rezultat je: <b>'+ x+'</b></p>'+
-        '<p>Tvoj odgovor je: <b>'+ y+'<b></p>'+
-        w+
-        '</div>'
-        ;
-        ocena.push(w);
-        $('.rezultati').append(rez_card);
-        $('#proveri_rezultat').prop('disabled',true);
-    });
+function getVal () {
+  $ ('.dugme').unbind ().on ('click', 'button', function () {
+    let rez = $ (this).closest ('div.sab').find ('input').val ();
+    $ (this).closest ('div.sab').find ('#potvrdi_odgovor').text (rez);
+    let broj1 = Number ($ (this).closest ('div.rand').find ('#broj_1').text ());
+    let broj2 = Number ($ (this).closest ('div.rand').find ('#broj_2').text ());
+    let broj3 = Number ($ (this).closest ('div.rand').find ('#broj_3').text ());
+    let objekat = new KarticaPodaci (
+      arr.length + 1,
+      [broj1, broj2, broj3],
+      rez
+    );
+    arr.push (objekat);
+  });
+}
 
-    // function oceniMe(){
-    //     let recenica = 'Odgovor je tacan!!';
-    //     let count = ocena.reduce((n, x) => n + (x === recenica), 0);
-    //     return count;
-    // }
-    arr.length = 0;
-    $('.ocena').append(k.oceniMe());
+function getSum (total, num) {
+  return total + Math.round (num);
+}
+
+function proveri (x) {
+  let k = new Kartica ();
+  x.forEach (element => {
+    let x = element._brojevi.reduce (getSum, 0);
+    let y = element._rez;
+    //let z = uporedi(x,y);
+    let w = k.proveriRezultat (x, y);
+    let rez_card =
+      '<div class="zadaci_sadrzaj card col " style="width: 18rem;">' +
+      '<h4>Zadatak broj ' +
+      element._zadatak +
+      '</h4>' +
+      '<p>Brojevi za sabiranje su: (' +
+      element._brojevi +
+      ')</p>' +
+      '<p>Tacan rezultat je: <b>' +
+      x +
+      '</b></p>' +
+      '<p>Tvoj odgovor je: <b>' +
+      y +
+      '<b></p>' +
+      w +
+      '</div>';
+    ocena.push (w);
+    $ ('.rezultati').append (rez_card);
+    $ ('#proveri_rezultat').prop ('disabled', true);
+  });
+
+  let ww = k.oceniMe ();
+  $ ('.ocena').append (ww);
+  arr.length = 0;
+  ocena.length = 0;
 }
